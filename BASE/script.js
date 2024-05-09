0f0f60bebe7d411b63639090c50e86a76a58018b
PocketProjectsHJC <vd900921@gmail.com>
2024-05-09 11:05:23 -0300

    Create script.js

---
+++ BASE/script.js
@@ -0,0 +1,17 @@
+const canvas=document.querySelector('canvas');
+const ctx=canvas.getContext('3d');
+canvas.addEventListener('touchmove',(e)=>{e.preventDefault()});
+function draw(){
+  
+}
+function update(){
+  
+}
+function event(){
+  
+}
+function init(){
+  setInterval(draw,10);
+  setInterval(update,5);
+}
+init();
