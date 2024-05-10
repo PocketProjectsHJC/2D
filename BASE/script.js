fe63e3465060d5864c3754502b813f131a5d02b4
PocketProjectsHJC <vd900921@gmail.com>
2024-05-10 19:53:28 -0300

    Atualizar o script.js

--- BASE/script.js
+++ BASE/script.js
@@ -1,14 +1,25 @@
 const canvas=document.querySelector('canvas');
 const ctx=canvas.getContext('2d');
-canvas.addEventListener('touchmove',(e)=>{e.preventDefault()});
+var touch=false;
+var touchPos=[0,0];
+canvas.addEventListener('touchstart',(e)=>{
+  touch=true;
+  touchPos=[e.touches[0].clientX,e.touches[0].clientY];
+});
+canvas.addEventListener('touchmove',(e)=>{
+  e.preventDefault();
+  touchPos=[e.touches[0].clientX,e.touches[0].clientY];
+});
+canvas.addEventListener('touchend',(e)=>{touch=false;});
 function draw(){
   
 }
 function update(){
   
+  event();
 }
 function event(){
-  
+
 }
 function init(){
   setInterval(draw,10);
